f1f44c5ef8c870bae07edc880589b758dc7f7196
flyyy26 <rfchannel0@gmail.com>
2025-03-25 11:50:58 +0700

    Fix

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error("❌ MONGODB_URI is not defined in .env.local");
-}
+// if (!MONGODB_URI) {
+//   throw new Error("❌ MONGODB_URI is not defined in .env.local");
+// }
 
 let cached = global.mongoose || { conn: null, promise: null };
 
